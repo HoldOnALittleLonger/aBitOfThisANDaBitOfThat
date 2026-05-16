6c3dc929ff1968f20631ce4deff00c7267aaa9a6
Zephyr <emailunspecified@false.com>
2026-05-16 14:11:27 +0800

    HontouHaMukuwarenainda.

---
+++ typedef_func_prototype.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * type alias for function signature.
+ * @print_something then can be used for
+ * function declaration.
+ * @print_something => void <func>(void)
+ */
+typedef void (print_something)(void);
+/**
+ * function declaration print_msg();
+ * @print_something => void <func>(void)
+ * @print_msg => <func>
+ * result => void print_msg(void)
+ */
+print_something print_msg;
+/**
+ * function declaration print_msg2();
+ */
+print_something print_msg2;
+
+/**
+ * function declaration for void print_msg3(void).
+ */
+void (print_msg3)(void);
+
+/**
+ * global function pointer initialized to
+ * print_msg().
+ */
+void (*print_msg_fp)(void) = print_msg;
+
+/**
+ * type alias for function pointer has
+ * signature void(void) .
+ */
+typedef void (*print_something_t)(void);
+
+int main(void)
+{
+        print_msg();
+        print_msg2();
+        print_msg3();
+
+        print_something_t pfp = print_msg;
+        print_msg_fp();
+        pfp();
+
+        return 0;
+}
+
+void print_msg(void)
+{
+        fprintf(stdout, "function %s is called\n", __FUNCTION__);
+}
+
+void print_msg2(void)
+{
+        fprintf(stdout, "function %s is called\n", __FUNCTION__);
+}
+
+void print_msg3(void)
+{
+        fprintf(stdout, "function %s is called\n", __FUNCTION__);
+}
+
